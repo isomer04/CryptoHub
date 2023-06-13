3149657c796a44eb65699b68379269098621f75c
isomer04 <isomer008@gmail.com>
2023-06-13 10:52:22 -0400

    Changed CryptoNews API

--- src/components/CryptoNews.jsx
+++ src/components/CryptoNews.jsx
@@ -1,55 +1,79 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "../App.css";
 
-const API_KEY = import.meta.env.VITE_APP_API_KEY_CRYPTO_NEWS;
+const API_KEY = import.meta.env.VITE_APP_API_KEY_CRYPTO;
 
 function CryptoNews() {
   const [newsList, setNewsList] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetchCryptoNews().catch(console.error);
-  }, []);
 
+
+  useEffect(() => {
+    fetchCryptoNews();
+  }, []);
+  
   const fetchCryptoNews = async () => {
-    const response = await fetch(
-      `https://newsapi.org/v2/everything?q=crypto&apiKey=${API_KEY}`
-    );
-    const data = await response.json();
-    if (data && data.articles) {
-      setNewsList(data.articles);
-      setLoading(false);
+    try {
+      const response = await axios.get(
+        `https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${API_KEY}`
+      );
+      const data = response.data.Data;
+      setNewsList(data);
+    } catch (error) {
+      console.error("Error fetching crypto news:", error);
     }
   };
+  
+
+  // return (
+  //   <div className="whole-page">
+  //     <h1>Crypto News</h1>
+  //     {loading ? (
+  //       <p>Loading...</p>
+  //     ) : (
+  //       <ul className="news-list">
+  //         {newsList.map((news, index) => (
+  //           news.urlToImage && (
+  //             <li key={index} className="news-item">
+  //               <div className="news-content">
+  //                 <img src={news.urlToImage} alt={news.title} />
+  //                 <div className="news-details">
+  //                   <h3>
+  //                     <a href={news.url} target="_blank" rel="noopener noreferrer">
+  //                       {news.title}
+  //                     </a>
+  //                   </h3>
+  //                   <p>{news.content}</p>
+  //                 </div>
+  //               </div>
+  //             </li>
+  //           )
+  //         ))}
+  //       </ul>
+  //     )}
+  //   </div>
+  // );
 
   return (
-    <div className="whole-page">
+    <div>
       <h1>Crypto News</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul className="news-list">
-          {newsList.map((news, index) => (
-            news.urlToImage && (
-              <li key={index} className="news-item">
-                <div className="news-content">
-                  <img src={news.urlToImage} alt={news.title} />
-                  <div className="news-details">
-                    <h3>
-                      <a href={news.url} target="_blank" rel="noopener noreferrer">
-                        {news.title}
-                      </a>
-                    </h3>
-                    <p>{news.content}</p>
-                  </div>
-                </div>
-              </li>
-            )
-          ))}
-        </ul>
-      )}
+      <ul>
+        {newsList.map((news) => (
+          <li key={news.id}>
+            <h3>{news.title}</h3>
+            <p>{news.body}</p>
+            <a href={news.url} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+  
 }
 
 export default CryptoNews;
